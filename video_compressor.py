bca525eca1b1d1f3af0db327f3e9d5e2d247bfe7
makim0939 <makim0939@gmail.com>
2025-10-05 17:13:04 +0900

    パラメータを追加。入出力用のフォルダを用意

--- video_compressor.py
+++ video_compressor.py
@@ -2,16 +2,21 @@ import glob
 import os
 import ffmpeg
 
-
+# 入出力先
 src_dir = "src_videos/"
 dst_dir = "dst_videos/"
 
-def compress_video(input_path, output_path, crf=30, scale_factor=1):
+# 圧縮パラメータ
+prm_crf = 30            # 画質
+prm_scale_factor = 1    # 解像度の圧縮率
+prm_fps = 30            # fps
+
+def compress_video(input_path, output_path, crf=30, scale_factor=1, fps=30):
     (
         ffmpeg
         .input(input_path)
         .filter('scale', f'iw*{scale_factor}', f'ih*{scale_factor}')
-        .output(output_path, vcodec='libx264', crf=crf, an=None)
+        .output(output_path, r=fps, vcodec='libx264', crf=crf, an=None)
         .run()
     )
 
@@ -27,7 +32,7 @@ def main():
             os.makedirs(dir_name)
         dst_name = dir_name + file_name + ".mp4"
         print("compress: " + file + "→" + dst_name)
-        compress_video(file, dst_name)
+        compress_video(file, dst_name, prm_crf, prm_scale_factor, prm_fps)
 
 if __name__ == "__main__":
     main()
